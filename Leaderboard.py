e49e9b9e75e6ecdcd83c31c6edd844fa8e1a827b
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-01 16:59:22 -0400

    Update Leaderboard.py

--- Leaderboard.py
+++ Leaderboard.py
@@ -10,39 +10,42 @@ class player:
         print(self.name, "Number of guesses: ", self.num_guess)
 
 
-class Leaderboard(player):
+class Leaderboard:
 
-    players = []
 
-    def __init__(self, wordlength, player, name, num_guess):
-        super.__init__(name, num_guess)
+    def __init__(self, wordlength, player):
         self.wordlength = wordlength
         self.player = player
+        self.players = []
 
-    def add_player(self, player):
-        players.append(self.player)
+    def add_player(self):
+        self.players.append(self.player)
+
+    def takenumguess(self, player):
+        return player.num_guess
 
     def sort_players(self, players):
-
+        sorted_list = sorted(players, key=self.takenumguess)
+        return sorted_list
 
     def display_board(self):
         print(self.wordlength, "Letter Words:\n")
-        for i in range(len(players)):
-            print(i + 1,") ",self.name, "Number of guesses", self.num_guess)
+        for i in range(len(self.players)):
+            print(i + 1,") ",self.player.name, "Number of guesses", self.player.num_guess)
 
-class total_leaderboard(Leaderboard(player)):
+class TotalLeaderboard:
 
-    final_board = []
 
-    def __init__(self,Leaderboard, wordlength, player, name, num_guess):
-        super.__init__(wordlength, player, name, num_guess)
-        self.leaderboard = Leaderboard
+    def __init__(self,leaderboard, wordlength):
+        self.leaderboard = leaderboard
+        self.wordlength = wordlength
+        self.final_board = []
 
-    def add_board(self, leaderboard):
-        final_board.append(self.leaderboard)
+    def add_board(self):
+        self.final_board.append(self.leaderboard)
         #sort so boards are in ascending order
 
     def display_total(self):
         print("Leaderboard:")
-        for i in range(len(final_board)):
+        for i in range(len(self.final_board)):
             Leaderboard.display_board()
