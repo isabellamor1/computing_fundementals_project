88d2a29d25ce12a56bb2fd8ebd2a174ece2db914
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-01 17:00:12 -0400

    Update Words.py

--- Words.py
+++ Words.py
@@ -9,6 +9,20 @@ class words:
         self.guess = guess
         self.word = word
 
+    def turn_green(self,letter):
+        """
+        Turns a given letter green and prints it
+        :return: printed green letter
+        """
+        print(Fore.GREEN + letter, end="")
+
+    def turn_yellow(self,letter):
+        """
+        Turns a given letter yellow and prints it
+        :return: printed yellow letter
+        """
+        print(Fore.YELLOW + letter, end="")
+
     def check_word(self):
         """
         Returns the users guess with the letter colors changed depending on their placement in the generated word
@@ -25,32 +39,12 @@ class words:
         else:
             for j in range(len(guess_letters)):
                 if guess_letters[j] in genword_letters and guess_letters[j] != genword_letters[j]:
-                    letter.turn_yellow(guess_letters[j])
+                    self.turn_yellow(guess_letters[j])
                 if guess_letters[j] == genword_letters[j]:
-                    letter.turn_green(guess_letters[j])
+                    self.turn_green(guess_letters[j])
                 elif guess_letters[j] not in genword_letters:
                     print(Fore.RESET + guess_letters[j], end="")
         return False
-
-
-class letter:
-
-    def __init__(self, letter):
-        self.letter = letter
-
-    def turn_green(self):
-        """
-        Turns a given letter green and prints it
-        :return: printed green letter
-        """
-        print(Fore.GREEN + self.letter, end="")
-
-    def turn_yellow(self):
-        """
-        Turns a given letter yellow and prints it
-        :return: printed yellow letter
-        """
-        print(Fore.YELLOW + self.letter, end="")
 
 
 class letter_number:
