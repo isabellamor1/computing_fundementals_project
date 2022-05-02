0d27eb4092992f058ad4f237617cfd16f1c99bb1
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-02 19:06:33 -0400

    Update Words.py

--- Words.py
+++ Words.py
@@ -1,121 +1,139 @@
-import colorama
+import random
 from colorama import Fore
-from time import sleep
-from Words import words
-from Words import letter_number as ln
-from Leaderboard import player
-from Leaderboard import imput_player
-from Leaderboard import display_boards
-from Leaderboard import Leaderboard
-
-colorama.init()
-
-person = player('name', 3, 3)
-board3 = Leaderboard(3, person)
-board4 = Leaderboard(4, person)
-board5 = Leaderboard(5, person)
-board6 = Leaderboard(6, person)
-board7 = Leaderboard(7, person)
 
 
-def instructions():
-    """
-    Prints out the game instructions
-    :return: Instructions
-    """
-    print("\nInstructions:")
-    print("\nFirst, choose how many letters you would like to be in your word by entering a number 3 to 7")
-    sleep(1.5)
-    print("\nNext, enter your first guess making sure to enter the correct number of letters")
-    sleep(1.5)
-    print("\nIf a letter in your guess is correct, the letter will turn green")
-    sleep(1.5)
-    ex_guess = "cat"
-    ex_word = "pot"
-    example = words(ex_guess, ex_word)
+class words:
 
-    print("\nExample:")
-    example.check_word()
-    sleep(1.5)
-    print(
-        Fore.RESET + "\nIf a letter in your guess is also in the word but not in the correct spot, the letter will "
-                     "turn yellow")
-    sleep(1.5)
-    print("\nExample:")
-    ex_guess = 'tin'
-    example = words(ex_guess, ex_word)
-    example.check_word()
-    sleep(1.5)
-    print("\nIf your guess is correct, a congratulatory message will appear")
-    sleep(1.5)
-    print(
-        "\nIf you do not correctly guess the word in the number of guesses you are given, you lose and the word will "
-        "be displayed")
-    sleep(1.5)
-    print("\nAfter you finish the game, you will have the option to play again\n")
-    sleep(1.5)
+    def __init__(self, guess, word):
+        self.guess = guess
+        self.word = word
+
+    def turn_green(self, letter):
+        """
+        Turns a given letter green and prints it
+        :return: printed green letter
+        """
+        print(Fore.GREEN + letter, end="")
+
+    def turn_yellow(self, letter):
+        """
+        Turns a given letter yellow and prints it
+        :return: printed yellow letter
+        """
+        print(Fore.YELLOW + letter, end="")
+
+    def check_word(self):
+        """
+        Returns the users guess with the letter colors changed depending on their placement in the generated word
+        :return: Players guess with the letters their corresponding colors
+        """
+
+        guess_letters = list(self.guess)
+        genword_letters = list(self.word)
+        while len(guess_letters) != len(genword_letters):
+            self.guess = input("Please enter the correct number of letters: ")
+            guess_letters = list(self.guess)
+        if guess_letters == genword_letters:
+            return True
+        else:
+            for j in range(len(guess_letters)):
+                if guess_letters[j] in genword_letters and guess_letters[j] != genword_letters[j]:
+                    self.turn_yellow(guess_letters[j])
+                if guess_letters[j] == genword_letters[j]:
+                    self.turn_green(guess_letters[j])
+                elif guess_letters[j] not in genword_letters:
+                    print(Fore.RESET + guess_letters[j], end="")
+        return False
+
+    def spell_check(self, num):
+        if num == '3':
+            with open("three.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                if self.guess not in all_words:
+                    return False
+                else:
+                    return True
+
+        if num == '4':
+            with open("four.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                if self.guess not in all_words:
+                    return False
+                else:
+                    return True
+
+        if num == '5':
+            with open("five.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                if self.guess not in all_words:
+                    return False
+                else:
+                    return True
+
+        if num == '6':
+            with open("six.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                if self.guess not in all_words:
+                    return False
+                else:
+                    return True
+
+        if num == '7':
+            with open("seven.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                if self.guess not in all_words:
+                    return False
+                else:
+                    return True
 
 
-loop = True
+class letter_number:
 
-print("Welcome to the word guessing game")
-p = input("Press 1 to play the game, 2 to view the instructions, or any other key to quit: ")
+    def __init__(self, num):
+        self.num = num
 
-if p != ('2' or '1' or '3'):
-    loop = False
-
-if p == '2':
-    instructions()
-    p = input("Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press 3): ")
-    loop = True
-
-if p == '1':
-    loop = True
-
-while loop:
-
-    if p == '2':
-        display_boards()
-        p = input("Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press 3): ")
-
-    if p == '1':
-        wordlength = input("Please enter the number of letters you want in your word (3-7): ")
-        num_class = ln(wordlength)
-        word = num_class.find_word()
-        guesses = int(wordlength) + 1
-        words_guessed = 0
-        for i in range(guesses):
-            guess = ''
-            if words_guessed == 0:
-                guess = input('Please enter your first guess: ')
-            elif words_guessed != 0:
-                guess = input(Fore.RESET + "\n\nEnter your next guess: ")
-            words_guessed += 1
-            game = words(guess, word)
-
-            # while not game.spell_check(wordlength):
-            #     guess = input('Please enter a real word: ')
-            #     game = words(guess, word)
-            #     game.spell_check(wordlength)
-
-            if game.check_word():
-                print("\nCongratulations, you guessed the word in", words_guessed, "guesses")
-                name = input("Please enter your name:")
-                plr = player(name, int(wordlength), int(words_guessed))
-                imput_player(plr, int(wordlength))
-                break
-            elif words_guessed == guesses:
-                print(Fore.RESET + '\nSorry, you are out of guesses')
-                print("The word was:", word)
-                loop = True
-            loop = True
-        p = input("Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press 3): ")
-
-    if p == '3':
-        print('Thanks for playing')
-        loop = False
-        break
-
-    # if p != '1' or '2' or '3':
-    #     p = input('Please enter 1, 2, or 3: ')
-    #     loop = True
+    def find_word(self):
+        """
+        Returns a randomly generated word with given correct number of letters
+        :return: word
+        """
+        if self.num == '3':
+            print("\nYou have 4 guesses")
+            with open("three.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                words = all_words.split()
+                word_num = random.randint(0, len(words))
+                fin_word = words[word_num]
+                return fin_word.lower()
+        if self.num == '4':
+            print("\nYou have 5 guesses")
+            with open("four.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                words = all_words.split()
+                word_num = random.randint(0, len(words))
+                fin_word = words[word_num]
+                return fin_word
+        if self.num == '5':
+            print("\nYou have 6 guesses")
+            with open("five.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                words = all_words.split()
+                word_num = random.randint(0, len(words))
+                fin_word = words[word_num]
+                return fin_word
+        if self.num == '6':
+            print("\nYou have 7 guesses")
+            with open("six.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                words = all_words.split()
+                word_num = random.randint(0, len(words))
+                fin_word = words[word_num]
+                return fin_word
+        if self.num == '7':
+            print("\nYou have 8 guesses")
+            with open("seven.txt", "r") as wordfile:
+                all_words = wordfile.read()
+                words = all_words.split()
+                word_num = random.randint(0, len(words))
+                fin_word = words[word_num]
+                return fin_word
