99b0a2f683a97fdd66e7f862877e13d7cfa1aeee
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-02 23:46:29 -0400

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,26 @@
+#this code can be inserted into main.py to test the leaderboard
+
+player1 = Player('Bill', 3, 4)
+imput_player(player1, 3)
+player2 = Player('Jerry', 3, 3)
+imput_player(player2, 3)
+player3 = Player('Bethany', 3, 2)
+imput_player(player3, 3)
+player7 = Player('Zoe', 6, 7)
+imput_player(player7, 6)
+player8 = Player('Jim', 5, 3)
+imput_player(player8, 5)
+player3 = Player('Lexy', 5, 5)
+imput_player(player3, 5)
+
+
+rand = 4
+player4 = Player('Bob', 4, 4)
+imput_player(player4, rand)
+player5 = Player('Larry', 4, 3)
+imput_player(player5, rand)
+player6 = Player('Barth', 4, 2)
+imput_player(player6, 4)
+player6 = Player('Paul', 4, 2)
+imput_player(player6, 4)
+
