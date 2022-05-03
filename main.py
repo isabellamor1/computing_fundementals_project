18116c98e6de512ec48185444d05b51bfa4a2cb1
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-02 23:43:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import colorama
 from colorama import Fore
 from time import sleep
-from Words import words
-from Words import letter_number as ln
-from Leaderboard import player
+from Words import Words
+from Words import LetterNumber as ln
+from Leaderboard import Player
 from Leaderboard import imput_player
 from Leaderboard import display_boards
 from Leaderboard import Leaderboard
 
 colorama.init()
 
-person = player('name', 3, 3)
+person = Player('name', 3, 3)
 board3 = Leaderboard(3, person)
 board4 = Leaderboard(4, person)
 board5 = Leaderboard(5, person)
@@ -32,7 +32,7 @@ def instructions():
     sleep(1.5)
     ex_guess = "cat"
     ex_word = "pot"
-    example = words(ex_guess, ex_word)
+    example = Words(ex_guess, ex_word)
 
     print("\nExample:")
     example.check_word()
@@ -43,7 +43,7 @@ def instructions():
     sleep(1.5)
     print("\nExample:")
     ex_guess = 'tin'
-    example = words(ex_guess, ex_word)
+    example = Words(ex_guess, ex_word)
     example.check_word()
     sleep(1.5)
     print("\nIf your guess is correct, a congratulatory message will appear")
@@ -52,7 +52,7 @@ def instructions():
         "\nIf you do not correctly guess the word in the number of guesses you are given, you lose and the word will "
         "be displayed")
     sleep(1.5)
-    print("\nAfter you finish the game, you will have the option to play again\n")
+    print("\nAfter you finish the game, you will have the option to play again")
     sleep(1.5)
 
 
@@ -64,7 +64,7 @@ p = input("Press 1 to play the game, 2 to view the instructions, or any other ke
 if p == '2':
     instructions()
     p = input(
-        "Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other key): ")
+        "\nWould you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other key): ")
     loop = True
 
 if p == '1':
@@ -75,13 +75,14 @@ while loop:
     if p == '2':
         display_boards()
         p = input(
-            "Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other key): ")
+            "\nWould you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other "
+            "key): ") 
 
     if p == '1':
-        wordlength = input("Please enter the number of letters you want in your word (3-7): ")
-        num_class = ln(wordlength)
+        word_length = input("Please enter the number of letters you want in your word (3-7): ")
+        num_class = ln(word_length)
         word = num_class.find_word()
-        guesses = int(wordlength) + 1
+        guesses = int(word_length) + 1
         words_guessed = 0
         for i in range(guesses):
             guess = ''
@@ -90,21 +91,22 @@ while loop:
             elif words_guessed != 0:
                 guess = input(Fore.RESET + "\n\nEnter your next guess: ")
             words_guessed += 1
-            game = words(guess, word)
+            game = Words(guess, word)
 
-            if game.spell_check(wordlength):
+            if game.spell_check(word_length):
                 pass
             else:
-                while not game.spell_check(wordlength):
-                    guess = input('Please enter a real word: ')
-                    game = words(guess, word)
-                    game.spell_check(wordlength)
+                while not game.spell_check(word_length):
+                    print('This word is not recognized by our systems')
+                    guess = input('Please enter a new word: ')
+                    game = Words(guess, word)
+                    game.spell_check(word_length)
 
             if game.check_word():
                 print("\nCongratulations, you guessed the word in", words_guessed, "guesses")
                 name = input("Please enter your name:")
-                plr = player(name, int(wordlength), int(words_guessed))
-                imput_player(plr, int(wordlength))
+                plr = Player(name, int(word_length), int(words_guessed))
+                imput_player(plr, int(word_length))
                 break
             elif words_guessed == guesses:
                 print(Fore.RESET + '\nSorry, you are out of guesses')
@@ -112,9 +114,5 @@ while loop:
                 loop = True
             loop = True
         p = input(
-            "Would you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other key): ")
-
-    # elif p != '1' or '2':
-    #     print('Thanks for playing')
-    #     loop = False
-    #     break
+            "\nWould you like to play a new game (press 1), view the leaderboard (press 2), or quit (press any other "
+            "key): ") 
