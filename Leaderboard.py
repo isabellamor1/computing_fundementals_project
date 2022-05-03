08f14d7f457226afd8faa6f8fed533467e08c123
isabellamor1 <103520863+isabellamor1@users.noreply.github.com>
2022-05-02 23:44:40 -0400

    Update Leaderboard.py

--- Leaderboard.py
+++ Leaderboard.py
@@ -1,13 +1,12 @@
+class Player:
 
-class player:
-
-    def __init__(self, name, wordlength, num_guess):
+    def __init__(self, name, word_length, num_guess):
         self.name = name
-        self.wordlegth = wordlength
+        self.wordlegth = word_length
         self.num_guess = num_guess
 
     def __str__(self):
-        return f'player({self.name}, {self.wordlegth}, {self.num_guess})'
+        return f'Player({self.name}, {self.wordlegth}, {self.num_guess})'
 
     def display_player(self):
         print(self.name, "Number of guesses: ", self.num_guess)
@@ -15,48 +14,42 @@ class player:
 
 class Leaderboard:
 
-    def __init__(self, wordlength, player):
-        self.wordlength = wordlength
-        self.player = player
-        self.players = []
+    def __init__(self, word_length, Player):
+        self.word_length = word_length
+        self.Player = Player
+        self.Players = []
 
     def __str__(self):
-        return f'(wordlength:{self.wordlength})'
+        return f'(word_length:{self.word_length})'
 
-    def add_player(self, player):
-        self.players.append(player)
+    def add_player(self, Player):
+        self.Players.append(Player)
 
-
-    def takenumguess(self, player):
-        return player.num_guess
-
-    def sort_players(self, players):
-        self.players.sort(key=lambda x: x.num_guess, reverse=False)
-
+    def sort_players(self):
+        self.Players.sort(key=lambda x: x.num_guess, reverse=False)
 
     def display_board(self):
-        if len(self.players) != 0:
-            Leaderboard.sort_players(self, self.players)
-            print('\nLeaderboard for', self.wordlength, "Letter Words:\n")
-            for i in range(len(self.players)):
-                self.player = self.players[i]
-                print(i + 1, ") ", self.player.name, "   |   Number of guesses:", self.player.num_guess)
+        if len(self.Players) != 0:
+            Leaderboard.sort_players(self)
+            print('\nLeaderboard for', self.word_length, "Letter Words:\n")
+            for i in range(len(self.Players)):
+                self.Player = self.Players[i]
+                print(i + 1, ") ", self.Player.name, "   |   Number of guesses:", self.Player.num_guess)
 
 
 class TotalLeaderboard:
 
-    def __init__(self, leaderboard, wordlength):
+    def __init__(self, leaderboard, word_length):
         self.leaderboard = leaderboard
-        self.wordlength = wordlength
+        self.word_length = word_length
         self.final_board = []
-        self.players = self.leaderboard.players
+        self.Players = self.leaderboard.Players
 
     def __str__(self):
-        return f'(wordlength: {self.wordlength})'
+        return f'(word_length: {self.word_length})'
 
     def add_board(self, leaderboard):
         self.final_board.append(leaderboard)
-        # sort so boards are in ascending order
 
     def display_total(self):
         for i in range(len(self.final_board)):
@@ -64,7 +57,7 @@ class TotalLeaderboard:
             Leaderboard.display_board(self.leaderboard)
 
 
-person = player('name', 3, 3)
+person = Player('name', 3, 3)
 board3 = Leaderboard(3, person)
 board4 = Leaderboard(4, person)
 board5 = Leaderboard(5, person)
@@ -72,17 +65,17 @@ board6 = Leaderboard(6, person)
 board7 = Leaderboard(7, person)
 
 
-def imput_player(player, num):
+def imput_player(Player, num):
     if num == 3:
-        board3.add_player(player)
+        board3.add_player(Player)
     if num == 4:
-        board4.add_player(player)
+        board4.add_player(Player)
     if num == 5:
-        board5.add_player(player)
+        board5.add_player(Player)
     if num == 6:
-        board6.add_player(player)
+        board6.add_player(Player)
     if num == 7:
-        board7.add_player(player)
+        board7.add_player(Player)
 
 
 totboard = TotalLeaderboard(board3, 3)
